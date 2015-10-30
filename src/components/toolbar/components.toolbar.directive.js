a613c64ca43fbd9bbafe531226867b285a0ae146
Pedro Gutiérrez <pgutierrez@stratio.com>
2015-10-30 13:27:18 +0100

    feature(childrenClass): Implemented childrenClass on Toolbar directive

--- src/components/toolbar/components.toolbar.directive.js
+++ src/components/toolbar/components.toolbar.directive.js
@@ -5,42 +5,16 @@
         .module('egeo.toolbar', [])
         .directive('egeoCToolbar', egeoCToolbar);
 
-    egeoCToolbar.$inject = ['EgeoConfig'];
+    egeoCToolbar.$inject = ['EgeoConfig', 'EgeoChildrenClass'];
 
-    function egeoCToolbar(EgeoConfig) {
-        function link(scope, elm, attrs, ctrl, transclude) {
-            // Create a temp store if not exists
-            if (!document.egeo) document.egeo = {};
-            if (!document.egeo.toolbars) document.egeo.toolbars = [];
-
-            // Save the element in a temp global item
-            document.egeo.toolbars.push(elm);
-
-            setTimeout(function(){ 
-                var toolbar;
-
-                // Retrieve the toolbar elements to put the subclass to
-                // its childs
-                for (var c = 0; c < document.egeo.toolbars.length; c++) {
-                    toolbar = document.egeo.toolbars[c];
-                    toolbar.children().addClass('egeo-c-toolbar__item');                
-                    document.egeo.toolbars.splice(c, 1);
-                }
-
-                // Free memory if not needed
-                
-                if (document.egeo.toolbars.length === 0) delete document.egeo.toolbars;
-                if (Object.keys(document.egeo).length === 0) delete document.egeo;
-            }, 0);
-        }
+    function egeoCToolbar(EgeoConfig, EgeoChildrenClass) {
 
         var directive = {
-            link: link,
+            link: EgeoChildrenClass('egeo-c-toolbar__item'),
             restrict: 'E',
             replace: true,
             transclude: true,
-            scope: {
-            },
+            scope: {},
             templateUrl: EgeoConfig.getEgeoPath() + '/components/toolbar/components.toolbar.tpl.html'
         }
 
