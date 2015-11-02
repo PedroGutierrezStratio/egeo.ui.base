4fbfaabf71e0d49b9a8316cbf927ff8f6a554222
alexrchies <alexrchies@gmail.com>
2015-11-02 10:05:30 +0100

    refactor(childrenClass Factory): change setTimeout to

--- src/factories/egeo.childrenClass.factory.js
+++ src/factories/egeo.childrenClass.factory.js
@@ -5,11 +5,12 @@
         .module('egeo.childrenClass', [])
         .factory('EgeoChildrenClass', EgeoChildrenClass);
 
-    function EgeoChildrenClass(){
+    EgeoChildrenClass.$inject = ['$timeout'];
+
+    function EgeoChildrenClass($timeout){
         return function(className){
             function link(scope, elem) {
-
-                setTimeout(function(){
+                $timeout(function(){
                     elem.children().addClass(className);
                 }, 0);
             }
