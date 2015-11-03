9f059aa514e2bb712647003cecb9bbe691183628
alexrchies <alexrchies@gmail.com>
2015-11-03 09:33:02 +0100

    feature(dropdown): add open/close behavior

--- src/components/dropdown/components.dropdown.directive.js
+++ src/components/dropdown/components.dropdown.directive.js
@@ -15,6 +15,7 @@
             transclude: true,
             scope: {
                 icon: '@',
+                isOpen: '@',
                 label: '@',
                 rounded: '@',
                 small: '@'
@@ -25,9 +26,19 @@
         return directive;
 
         function link(scope, elem) {
+            // Initialize the status if the user didn't set it via parameter
+            if (!scope.isOpen) scope.isOpen = false;
+
+            elem.find('egeo-c-button').bind('click', onClickDropdownBtn);
+
             setTimeout(function() {
+                // Apply the item class to all elements inside the menu
                 elem.find('.egeo-c-dropdown__menu').children().addClass('egeo-c-dropdown__item');
             }, 0);
+        }
+
+        function onClickDropdownBtn() {
+            scope.isOpen = !scope.isOpen;
         }
     }
 })();
