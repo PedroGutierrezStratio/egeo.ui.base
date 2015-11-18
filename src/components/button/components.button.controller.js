2e9346d4391a1139dc1d020c3f5a66f2fdf193a4
alexrchies <alexrchies@gmail.com>
2015-11-18 11:02:43 +0100

    fix(popover): add missed css

--- src/components/button/components.button.controller.js
+++ src/components/button/components.button.controller.js
@@ -22,11 +22,15 @@
 
         function openPopover() {
             angular.element($element.parent().find('[data-id="' + $scope.popover + '"]')).addClass('egeo-c-popover--open');
+            angular.element($element.find('.egeo-c-popover__arrow')).addClass('egeo-c-popover__arrow--open');
+            angular.element($element.find('.egeo-c-popover__arrow-shadow')).addClass('egeo-c-popover__arrow-shadow--open');
             vm.isOpenPopover = true;
         }
 
         function closePopover() {
             angular.element($element.parent().find('[data-id="' + $scope.popover + '"]')).removeClass('egeo-c-popover--open');
+            angular.element($element.find('.egeo-c-popover__arrow')).removeClass('egeo-c-popover__arrow--open');
+            angular.element($element.find('.egeo-c-popover__arrow-shadow')).removeClass('egeo-c-popover__arrow-shadow--open');
             vm.isOpenPopover = false;
         }
     }
